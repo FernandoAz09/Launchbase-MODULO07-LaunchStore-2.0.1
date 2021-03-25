d8c6a0dd5fb211f854d1e761978561b703601efa
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-03-25 19:40:31 -0300

    Login + Rotas de login liberadas

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,13 @@
 module.exports = {
-      logout(req, res) {
-          req.session.destroy()
-          return res.redirect("/")
-      }
+    loginForm(req, res) {
+        return res.render("session/login")
+    },
+    login(req, res) {
+        req.session.userId = req.user.id
+        return res.redirect("/users")
+    },
+    logout(req, res) {
+        req.session.destroy()
+        return res.redirect("/")
+    }
 }
